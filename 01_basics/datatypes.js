c72c09fbd83e4729b9d7849d6f099766bf704a14
Anushka Mishra <165766043+anush-mish@users.noreply.github.com>
2024-04-26 09:29:02 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,37 @@
+"use strict";//treat all js code as newer version of javascript
+
+// alert(3+3)   it will not run here because we are here using node js not browser
+
+//the way mentioned below reduces the readability of code, therefore it is not preferable to use it:-
+
+console.log(3+
+3);
+
+console.log(6); console.log("hitesh")
+let name="Anushka";
+let age=18;
+let isLoggedin=false;
+let monster;
+let tatti=null;
+let potty;
+console.log(name);//deprecated 
+console.log(age);
+console.log(isLoggedin);
+console.log(monster);
+console.log(tatti);
+
+// number limit => 2 to the power 53
+// BigInt
+// boolean
+// string
+// null=>standalone value
+// undefined is the situation whwn you have not given any value
+// symbol =>uniqueness finding tool (will be used in react)
+
+console.log(typeof tatti);//so null is having the datatype as object
+
+//type of undefined is null
+
+console.log(potty);
+console.log(typeof(potty));
+
