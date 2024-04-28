e5eb5478ef452c18a60831e2f9e2bdf93d6730d1
Anushka Mishra <165766043+anush-mish@users.noreply.github.com>
2024-04-28 09:39:05 +0000

    sucide

--- 01_basics/datatypes_summay.js
+++ 01_basics/datatypes_summay.js
@@ -18,3 +18,42 @@ console.log(id==newid);//false
 // non primitive datatypes(passed by reference):-
 // Array,Objects,Functions
 
+// In JavaScript, when you append n to the end of a number, like 12333545463645n, you're explicitly declaring it as a BigInt.4
+// let bigNumber=12345677797969866543
+;//output=>12345677797969867000
+
+// const bigNumber=12345677797969866543n;
+// console.log(bigNumber);
+console.log(typeof bigNumber);//why this works???
+
+//array:-
+const heros=["Shaktiman","Nagraj","Doga"];
+console.log(heros[0]);
+console.log(heros[1]);
+console.log(heros[2]);
+
+let myob={
+    Name:"Anushka",
+    Class:"12th",
+    innerobj:{
+        newNum:123,
+        oldNum:456,
+    }
+};
+
+console.log(myob.Name);
+console.log(myob["Name"]);
+console.log(myob.innerobj);
+console.log(myob.innerobj.newNum);
+
+const myFunc=function(){
+    console.log("hello world");
+}
+
+myFunc();//function call in this manner+
+
+
+console.log(typeof myFunc);//function (although called as object function )
+
+console.log(typeof heros);//object
+
